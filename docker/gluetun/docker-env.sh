58ea06261f691c663916899bcda5af62684af177
Greg Robinson <gregrob@mac.com>
2026-04-10 10:52:07 +1000

    initial integration of gluetun stack

---
+++ docker/gluetun/docker-env.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Include the secrets helper script to use get_secret function
+source "$(dirname "${BASH_SOURCE[0]}")/../secrets-helper.sh"
+
+# Check if the script is being sourced
+if [ "${BASH_SOURCE[0]}" != "${0}" ]
+then
+
+    echo "----------------------------------------------"
+    echo "Setting up SPECIFIC environment for docker ..."
+    echo "----------------------------------------------"
+    
+    export ENV_WIREGUARD_PRIVATE_KEY_GLUETUN_001=$(get_secret "apps/gluetun/gluetun-wireguard-private-key-001.secret.age")
+    echo "Exported ENV_WIREGUARD_PRIVATE_KEY_GLUETUN_001=$ENV_WIREGUARD_PRIVATE_KEY_GLUETUN_001"
+
+    export ENV_WIREGUARD_PRIVATE_KEY_GLUETUN_002=$(get_secret "apps/gluetun/gluetun-wireguard-private-key-002.secret.age")
+    echo "Exported ENV_WIREGUARD_PRIVATE_KEY_GLUETUN_002=$ENV_WIREGUARD_PRIVATE_KEY_GLUETUN_002"
+
+    export ENV_QBITTORRENT_UI_PORT_PUB=8080
+    echo "Exported ENV_QBITTORRENT_UI_PORT_PUB=$ENV_QBITTORRENT_UI_PORT_PUB"
+    
+    export ENV_QBITTORRENT_UI_PORT_PRV=8081
+    echo "Exported ENV_QBITTORRENT_UI_PORT_PRV=$ENV_QBITTORRENT_UI_PORT_PRV"
+
+    echo ""
+
+else
+    echo "FAIL: Please call script with - source ./env.sh"
+
+    exit 1
+
+fi
