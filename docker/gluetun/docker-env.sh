77fca7bb34e31a0f74571f2e7421643621712f2b
Greg Robinson <gregrob@mac.com>
2026-04-10 16:08:55 +1000

    added prowlarr and flaresolverr to the stack

--- docker/gluetun/docker-env.sh
+++ docker/gluetun/docker-env.sh
@@ -23,6 +23,9 @@ then
     export ENV_QBITTORRENT_UI_PORT_PRV=8081
     echo "Exported ENV_QBITTORRENT_UI_PORT_PRV=$ENV_QBITTORRENT_UI_PORT_PRV"
 
+    export ENV_PROWLARR_PORT=9696
+    echo "Exported ENV_PROWLARR_PORT=$ENV_PROWLARR_PORT"
+
     echo ""
 
     echo "To test that our VPN is correctly routing our QBittorrent traffic, we can download a test torrent and check our IP."
